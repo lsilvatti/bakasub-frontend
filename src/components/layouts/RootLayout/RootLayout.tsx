a4ff47830f0bd011b9d65f40e06c44d3268034a5
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-21 21:11:18 -0300

    feat: add ThemeToggle component to RootLayout for theme switching

--- src/components/layouts/RootLayout/RootLayout.tsx
+++ src/components/layouts/RootLayout/RootLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import styles from './RootLayout.module.css';
 import Typography from "@/components/atoms/Typography/Typography";
+import ThemeToggle from "@/components/atoms/ThemeToggle";
 
 export function RootLayout() {
     return (
@@ -8,6 +9,7 @@ export function RootLayout() {
             <header>
                 <Typography>
                     Bakasub
+                    <ThemeToggle />
                 </Typography>
             </header>
             
