da8b8c901eec7d0076f20437ff9fe2fbe7e7e369
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-24 18:58:45 -0300

    feat: add new page components for Config, Extract, Logs, Merge, and Translate; update Home component export and modify router configuration

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,16 +1,18 @@
-import { createBrowserRouter } from "react-router-dom";
-import { RootLayout } from '@/components/layouts'
-import { Home } from "./pages";
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import { RootLayout } from '@/components/templates';
+import { ErrorBoundary } from '@/components/organisms';
+import { APP_ROUTES } from '@/config/routes';
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <RootLayout />,
+        errorElement: <ErrorBoundary />,
         children: [
-            { 
-                path: '/',
-                element: <Home />
-            }
-        ]
-    }
-])
+            ...APP_ROUTES.map(route => ({
+                path: route.path,
+                element: route.element,
+            }))
+        ],
+    },
+]);
