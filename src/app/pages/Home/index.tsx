b8ee171a04f887b523c55ea5e15900c7df3f71f6
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-24 10:45:32 -0300

    feat: update Home component and refine global styles for header and text

--- src/app/pages/Home/index.tsx
+++ src/app/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 export function Home () {
     return (
         <>
-            <h1>HomePage</h1>
         </>
     )
 }
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,2 +1,5 @@
 export { ThemeToggle } from './ThemeToggle';
 export { Typography } from './Typography';
+export { IconButton } from './IconButton';
+export { Overlay } from './Overlay';
+export { NavItem } from './NavItem';
