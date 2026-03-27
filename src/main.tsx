e7aa57ba5e7a223d836a18104086af170f493feb
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-27 14:04:58 -0300

    feat: refactor router and main components to use Loading component in Suspense and wrap RouterProvider with QueryClientProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@/lib/i18n';
-
-import { router } from '@/app/router'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 
+import '@/lib/i18n';
+import { router } from '@/app/router'
 import './globals.css'
 import ThemeProvider from './contexts/ThemeContext'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1, 
+    },
+  },
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-       <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+         <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
