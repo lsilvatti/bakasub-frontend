5d9a91f1d8cf98a7bc0cf32612c0548c6ab3381c
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-03-28 19:18:06 -0300

    feat: add Translate module CSS and remove obsolete Translate component

--- src/app/pages/Translate/Translate.tsx
+++
@@ -1,9 +0,0 @@
-import { PageTitle } from "@/components/atoms/PageTitle";
-
-export default function Translate () {
-    return (
-        <>
-            <PageTitle titleKey={"pages.translate.title"} />
-        </>
-    )
-}
