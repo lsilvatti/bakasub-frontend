33118bc898c5f6a4bd9598f95b574e2f0ef61cdf
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-04-01 19:32:07 -0300

    feat: refactor ModelsPresetsLanguages layout to use SplitPageLayout and enhance styling for models and presets stacks

--- src/app/pages/ModelsPresetsLanguages/index.tsx
+++ src/app/pages/ModelsPresetsLanguages/index.tsx
@@ -1,18 +1,29 @@
-import { ThreeColumnLayout } from '@/components/templates';
+import { SplitPageLayout } from '@/components/templates';
 import styles from './ModelsPresetsLanguages.module.css';
 
 import ModelsColumn from './components/ModelsColumn';
+import FavoritesTable from './components/FavoritesTable';
+import PresetsColumn from './components/PresetsColumn';
+import LanguagesColumn from './components/LanguagesColumn';
 
 export default function ModelsPresetsLanguages() {
 
     return (
-        <ThreeColumnLayout
+        <SplitPageLayout
+            variant='half'
             titleKey="pages.modelsPresetsLanguages.title"
-            columns={[
-                <ModelsColumn key="models" />,
-                null,
-                null
-            ]}
+            leftContent={
+                <div key="models" className={styles.modelsStack}>
+                    <ModelsColumn />
+                    <FavoritesTable />
+                </div>
+            }
+            rightContent={
+                <div key="models" className={styles.presetsStack}>
+                    <PresetsColumn key="presets" />
+                    <LanguagesColumn key="languages" />
+                </div>
+            }
             layoutClassName={styles.layout}
         />
     );
