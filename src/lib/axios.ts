cd885597a2de276265f9eb95ec17204222edbb92
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-04-08 15:34:11 -0300

    feat: update base URL in axios configuration and add session job management functions

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1/';
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api/v1/';
 
 export const api = axios.create({
   baseURL: baseURL,
