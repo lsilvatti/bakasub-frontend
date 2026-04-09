d4d983c7234d3e1e80f22a479faab7c4e7eb71b9
Leonardo Silvatti <leosilvatti0@gmail.com>
2026-04-08 22:36:33 -0300

    feat: filter APP_ROUTES to exclude separators in router configuration

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { RootLayout } from '@/components/templates';
 import { ErrorBoundary } from '@/components/organisms';
-import { APP_ROUTES } from '@/config/routes';
+import { APP_ROUTES, isSeparator } from '@/config/routes';
 import { Suspense } from 'react';
 import Loading from './pages/Loading';
 
@@ -11,9 +11,9 @@ export const router = createBrowserRouter([
         element: <RootLayout />,
         errorElement: <ErrorBoundary />,
         children: [
-            ...APP_ROUTES.map(route => ({
-                path: route.path,
-                element: <Suspense fallback={<Loading />}>{route.element}</Suspense>,
+            ...APP_ROUTES.filter(r => !isSeparator(r)).map(route => ({
+                path: (route as any).path,
+                element: <Suspense fallback={<Loading />}>{(route as any).element}</Suspense>,
             }))
         ],
     },
